7ca8b4470b10ff7e412590391ff0292cb1ba56cc
geoseo360 <geoseo360@gmail.com>
2024-04-23 01:44:25 +0400

    Commit

---
+++ data.js
@@ -0,0 +1,313 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Прихожая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.5504931710027918,
+        "pitch": 0.013677281636217842,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9129273090763403,
+          "pitch": 0.13221231203374906,
+          "rotation": 0,
+          "target": "5-2-"
+        },
+        {
+          "yaw": 1.4513124573399603,
+          "pitch": 0.0614499894545375,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.0858970546301983,
+          "pitch": 0.11229008362021986,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.0196922906064607,
+        "pitch": 0.04833878702369532,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1808307088429899,
+          "pitch": 0.08811695623330706,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.4778862709868044,
+          "pitch": 0.1396298979485131,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Гостинная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.49063357606037883,
+        "pitch": 0.11632821514811198,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11404013350102638,
+          "pitch": 0.2726168029421032,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.1196736567998329,
+          "pitch": 0.07338753367084472,
+          "rotation": 4.71238898038469,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.3464451332542424,
+          "pitch": 0.023811938685593503,
+          "rotation": 0,
+          "target": "5-2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Кухня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.3837108798545685,
+        "pitch": 0.1576919193731925,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.579687842438183,
+          "pitch": 0.061171053401396946,
+          "rotation": 5.497787143782138,
+          "target": "5-2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "Ванная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.00888876459961807,
+        "pitch": -0.006771985455133134,
+        "fov": 1.3069931928408902
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.863583739848197,
+          "pitch": 0.09695376781827392,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2-",
+      "name": "2 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.5022631568288283,
+        "pitch": -0.02585071447736098,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1757620858080422,
+          "pitch": 0.1351004958634121,
+          "rotation": 0,
+          "target": "6-2-"
+        },
+        {
+          "yaw": 2.5626452839678917,
+          "pitch": 0.23192130348529538,
+          "rotation": 2.356194490192345,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-2-",
+      "name": "2 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.9832021452901394,
+        "pitch": 0.0646267861933918,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16735209381525884,
+          "pitch": 0.1418477618779459,
+          "rotation": 2.356194490192345,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Batumi-Vaja-Pshavela",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
